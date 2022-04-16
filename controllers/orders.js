97df304d262b058dc2c0076bb995a09956c0b4ba
Samyak Panchbhai <samyakmp111@gmail.com>
2022-04-16 15:44:44 +0530

    updating

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,7 +4,11 @@ const Razorpay = require("razorpay");
 
 const createNewOrder = async (req, res) => {
   try {
-    const instance = new Razorpay({
+    // const instance = new Razorpay({
+    //   key_id: process.env.RAZORPAY_KEY_ID,
+    //   key_secret: process.env.RAZORPAY_SECRET,
+    // });
+    var instance = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_SECRET,
     });
@@ -12,7 +16,7 @@ const createNewOrder = async (req, res) => {
       amount: req.body.amount * 100,
       currency: "INR",
       reciept: req.body.receipt,
-      payment_capture: 1,
+      //payment_capture: 1,
     };
     const order = await instance.orders.create(options);
     if (!order) return res.status(500).send("Some error occured");
