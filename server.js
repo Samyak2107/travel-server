95dae0a09c06dc10fa1bc79e1a1c865a83e96b3e
Samyak Panchbhai <samyakmp111@gmail.com>
2022-04-16 13:47:01 +0530

    added razorpay routes

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const User = require("./models/user");
 const cors = require("cors");
 
 const app = express();
+
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 const domainsFromEnv = process.env.CORS_DOMAINS || "";
